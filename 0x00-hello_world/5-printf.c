4888f53276266d1aaa7789d39632f0b5254952df
Khayfire <kayode354@gmail.com>
2022-09-08 19:31:34 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-serves as the entry poin tof a program
+ *
+ * Return:if no error return zero, otherwise return non zero value
+ */
+
+ int main(void)
+ {
+    printf("with proper grammar, but the outcome is a piece of art,\n.");
+    return (0);
+ }
